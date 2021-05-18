2de2b6b5b41f19ef0474faaee3ba6aff2fc1723c
DevilToast <bbq2277@naver.com>
2021-05-19 04:17:32 +0900

    Cat Like Coding Movement Chapter 1. 그냥 잠깐만 입력하고 테스트용으로 올림

---
+++ CLC_Movement_withGit/Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Player Input
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        transform.localPosition = new Vector3(playerInput.x, .5f, playerInput.y);
+    }
+}
