2d6749e03a17de779f2bb0bd12883a2d14079a36
DevilToast <bbq2277@naver.com>
2021-05-19 04:45:26 +0900

    2편까지는 함. MoveTowards로 desiredVelocity 적용

--- CLC_Movement_withGit/Assets/Scripts/MovingSphere.cs
+++ CLC_Movement_withGit/Assets/Scripts/MovingSphere.cs
@@ -7,6 +7,9 @@ public class MovingSphere : MonoBehaviour
     [SerializeField, Range(0f, 100f)]
     float maxSpeed = 10f;
 
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
     Vector3 velocity;
 
     // Start is called before the first frame update
@@ -28,8 +31,13 @@ public class MovingSphere : MonoBehaviour
 
         // 값 저장을 위해 velocity를  전역변수화
         // Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        Vector3 acceleration = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        velocity += acceleration * Time.deltaTime;
+
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
     }
