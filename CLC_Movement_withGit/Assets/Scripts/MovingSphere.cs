946bb68f8b134b9c0c7018fba77470648e41c70a
DevilToast <bbq2277@naver.com>
2021-05-20 20:39:26 +0900

    Rect를 이용해서 이동 범위를 제한

--- CLC_Movement_withGit/Assets/Scripts/MovingSphere.cs
+++ CLC_Movement_withGit/Assets/Scripts/MovingSphere.cs
@@ -10,6 +10,10 @@ public class MovingSphere : MonoBehaviour
     [SerializeField, Range(0f, 100f)]
     float maxAcceleration = 10f;
 
+    // ground의 이동 바운더리 설정
+    [SerializeField]
+    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
     Vector3 velocity;
 
     // Start is called before the first frame update
@@ -39,6 +43,15 @@ public class MovingSphere : MonoBehaviour
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
         Vector3 displacement = velocity * Time.deltaTime;
-        transform.localPosition += displacement;
+
+        // local Position에 바로 접근해도 되지만, 변수를 한 번 거쳐서 판단
+        Vector3 newPosition = transform.localPosition + displacement;
+        // 오버로딩 형을 맞추기 위해, 인자는 Vector2로
+        if(!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+        {
+            newPosition = transform.localPosition;
+        }
+
+        transform.localPosition = newPosition;
     }
 }
