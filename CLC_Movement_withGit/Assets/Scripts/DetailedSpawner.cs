efd7c4e4c51991780ab2784c4c71630a17d46cdc
DevilToast <bbq2277@naver.com>
2021-05-24 14:05:34 +0900

    2.3 - 2.2 Layer Mask 테스트

---
+++ CLC_Movement_withGit/Assets/Scripts/DetailedSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailedSpawner : MonoBehaviour
+{
+
+    public GameObject detailed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 39; i < 68; i++)
+            Instantiate(detailed, new Vector3(i, 27, 60), detailed.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
