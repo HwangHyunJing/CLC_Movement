da4d0eb2f11677be5e66bec3fdba2bf4ac05b60c
HwangHyunJing <bbq2277@naver.com>
2021-05-28 15:16:40 +0900

    2.6 - 1.* 일단 새로운 중력원 만들기는 했는데 테스트 결과가 좀 시원찮음...

--- CLC_Movement_withGit/Assets/Scripts/CustomGravity.cs
+++ CLC_Movement_withGit/Assets/Scripts/CustomGravity.cs
@@ -4,32 +4,80 @@ using UnityEngine;
 
 public static class CustomGravity
 {
+    // 복수개의 중력을 다루기 위한 List
+    static List<GravitySource> sources = new List<GravitySource>();
+
     // 월드 좌표에 따른 중력을 리턴
     public static Vector3 GetGravity(Vector3 position)
     {
-        // return Physics.gravity;
-        return position.normalized * Physics.gravity.y;
+        // List의 중력원들을 하나씩 더함
+        Vector3 g = Vector3.zero;
+        for(int i=0; i < sources.Count; i++)
+        {
+            
+            g += sources[i].GetGravity(position);
+        }
+
+        // return position.normalized * Physics.gravity.y;
+        // 리턴값은 방향 성분만이 아니라 크기도 있으므로, 정규화 안하는게 맞다
+        return g;
     }
 
     // 구체와 orbit 카메라에 따라서 upAxis를 리턴
     public static Vector3 GetUpAxis(Vector3 position)
     {
-        // 여러가지 중력을 적용할 수 있도록 함
-        Vector3 up = position.normalized;
+        // position 대신 source 기반
+        Vector3 g = Vector3.zero;
+        for(int i=0; i < sources.Count; i++)
+        {
+            g += sources[i].GetGravity(position);
+        }
 
-        // return -Physics.gravity.normalized;
-        // return position.normalized;
-        return Physics.gravity.y < 0f ? up : -up;
+        // up Axis 즉 방향을 리턴
+        return -g.normalized;
     }
 
     // out 파라미터를 이용해 upAxis까지 리턴하는 GetGravity 오버로딩형
     public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
     {
-        // 기본적인 중력인 Physics.gravity 대신에 position 기반으로 진행
+        // position 기반 대신에 sources List 기반으로 진행
+        Vector3 g = Vector3.zero;
+        for(int i=0; i < sources.Count; i++)
+        {
+            g += sources[i].GetGravity(position);
+        }
+
+        upAxis = -g.normalized;
+        return g;
+
+        /*
         Vector3 up = position.normalized;
 
         // upAxis = -Physics.gravity.normalized;
         upAxis = Physics.gravity.y < 0f ? up : -up;
         return up * Physics.gravity.y;
+        */
+    }
+
+    // 중력원을 추가
+    public static void Register(GravitySource source)
+    {
+        // 이미 있는 중력원을 또 추가하려 하면 에러 메시지를 띄움
+        // assert의 bool 조건은, '되기를 바라는, 예상하는' 조건입니다
+        Debug.Assert(
+            !sources.Contains(source),
+            "Duplicated registration of gravity source!", source);
+
+        sources.Add(source);
+    }
+
+    // 중력원을 제거
+    public static void Unregister(GravitySource source)
+    {
+        Debug.Assert(
+            sources.Contains(source),
+            "Unregisteration of unkown  gravity source!", source);
+
+        sources.Remove(source);
     }
 }
---
+++ CLC_Movement_withGit/Assets/Scripts/GravitySource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySource : MonoBehaviour
+{
+    // 가상 메소드
+    public virtual Vector3 GetGravity (Vector3 position)
+    {
+        return Physics.gravity;
+    }
+
+    // 스스로를 추가 및 해제하는 메소드
+    // onEnable, onDisable은 물체의 active/inactive에 따라 자동으로 호출
+    private void OnEnable()
+    {
+        CustomGravity.Register(this);
+    }
+
+    private void OnDisable()
+    {
+        CustomGravity.Unregister(this);
+    }
+}
