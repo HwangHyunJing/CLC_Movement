090b19988a0c7043bc4a14ac08401a8243c4a4fd
HwangHyunJing <bbq2277@naver.com>
2021-06-02 22:06:49 +0900

    2.6 끝. 상자 중력까지 만들었다

--- CLC_Movement_withGit/Assets/Scripts/GravityBox.cs
+++ CLC_Movement_withGit/Assets/Scripts/GravityBox.cs
@@ -19,8 +19,12 @@ public class GravityBox : GravitySource
     [SerializeField, Min(0f)]
     float innerDistance = 0f, innerFalloffDistance = 0f;
 
+    // 상자의 '외부' 중력을 설정
+    [SerializeField, Min(0f)]
+    float outerDistance = 0f, outerFalloffDistance = 0f;
+
     // 거리에 따른 중력 크기 차감
-    float innerFalloffFactor;
+    float innerFalloffFactor, outerFalloffFactor;
 
     private void Awake()
     {
@@ -39,7 +43,11 @@ public class GravityBox : GravitySource
         innerDistance = Mathf.Min(innerDistance, maxInner);
         innerFalloffDistance = Mathf.Max(Mathf.Min(innerFalloffDistance, maxInner), innerDistance);
 
+        // outer 값에 대해서도 최대, 최소 조정
+        outerFalloffDistance = Mathf.Max(outerFalloffDistance, outerDistance);
+
         innerFalloffFactor = 1f / (innerFalloffDistance - innerDistance);
+        outerFalloffFactor = 1f / (outerFalloffDistance - outerDistance);
     }
 
     private void OnDrawGizmos()
@@ -67,6 +75,74 @@ public class GravityBox : GravitySource
 
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(Vector3.zero, 2f * boundaryDistance);
+
+        // 각각 노란색, 파란색으로 칠한다
+        if(outerDistance > 0f)
+        {
+            Gizmos.color = Color.yellow;
+            DrawGizmosOuterCube(outerDistance);
+        }
+        if(outerFalloffDistance > outerDistance)
+        {
+            Gizmos.color = Color.cyan;
+            DrawGizmosOuterCube(outerFalloffDistance);
+        }
+    }
+
+    // 라이브러리 아님!
+    // 4개의 점을 잇는 직사각형을 그림
+    // 상자의 각 면에 대해한 matrix4x4를 직접 접근할 수 없으므로
+    void DrawGizmosRect(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+    {
+        Gizmos.DrawLine(a, b);
+        Gizmos.DrawLine(b, c);
+        Gizmos.DrawLine(c, d);
+        Gizmos.DrawLine(d, a);
+    }
+
+    void DrawGizmosOuterCube (float distance)
+    {
+        Vector3 a, b, c, d;
+
+        // boundary distance의 단순한 연장선
+        a.y = b.y = boundaryDistance.y;
+        d.y = c.y = -boundaryDistance.y;
+        b.z = c.z = boundaryDistance.z;
+        d.z = a.z = -boundaryDistance.z;
+        a.x = b.x = c.x = d.x = boundaryDistance.x + distance;
+
+        // 일단 하나를 그리고
+        DrawGizmosRect(a, b, c, d);
+
+        // x좌표에 대해서 반전
+        a.x = b.x = c.x = d.x = -a.x;
+        DrawGizmosRect(a, b, c, d);
+
+        a.x = d.x = boundaryDistance.x;
+        b.x = c.x = -boundaryDistance.x;
+        a.z = b.z = boundaryDistance.z;
+        c.z = d.z = -boundaryDistance.z;
+        a.y = b.y = c.y = d.y = boundaryDistance.y + distance;
+        DrawGizmosRect(a, b, c, d);
+        a.y = b.y = c.y = d.y = -a.y;
+        DrawGizmosRect(a, b, c, d);
+
+        a.x = d.x = boundaryDistance.x;
+        b.x = c.x = -boundaryDistance.x;
+        a.y = b.y = boundaryDistance.y;
+        c.y = d.y = -boundaryDistance.y;
+        a.z = b.z = c.z = d.z = boundaryDistance.z + distance;
+        DrawGizmosRect(a, b, c, d);
+        a.z = b.z = c.z = d.z = -a.z;
+        DrawGizmosRect(a, b, c, d);
+
+        distance *= 0.57f; // 0.57은 3의 제곱근 근사값입니다
+        Vector3 size = boundaryDistance;
+        size.x = 2f * (size.x + distance);
+        size.y = 2f * (size.y + distance);
+        size.z = 2f * (size.z + distance);
+        Gizmos.DrawWireCube(Vector3.zero, size);
+
     }
 
     // coordinate는 position의 '상대적인 위치'
@@ -99,6 +175,62 @@ public class GravityBox : GravitySource
         position = transform.InverseTransformDirection(position - transform.position);
         Vector3 vector = Vector3.zero;
 
+        int outside = 0;
+        // 중심부보다 멀어서 경계도 벗어나는 경우
+        if(position.x > boundaryDistance.x)
+        {
+            vector.x = boundaryDistance.x - position.x;
+            outside += 1;
+        }
+        // 반대쪽 끝
+        else if (position.x < -boundaryDistance.x)
+        {          
+            vector.x = -boundaryDistance.x - position.x;
+            outside += 1;
+        }
+
+        if(position.y > boundaryDistance.y)
+        {
+            vector.y = boundaryDistance.y - position.y;
+            outside += 1;
+        }
+        else if(position.y < -boundaryDistance.y)
+        {
+            vector.y = -boundaryDistance.y - position.y;
+            outside += 1;
+        }
+
+        if (position.z > boundaryDistance.z)
+        {
+            vector.z = boundaryDistance.z - position.z;
+            outside += 1;
+        }
+        else if (position.z < -boundaryDistance.z)
+        {
+            vector.z = -boundaryDistance.z - position.z;
+            outside += 1;
+        }
+
+
+        // 구체가 박스 밖에 있는 경우
+        if(outside > 0)
+        {
+            float distance = (outside==1 ? Mathf.Abs(vector.x + vector.y + vector.z) : vector.magnitude);
+            if(distance > outerFalloffDistance)
+            {
+                return Vector3.zero;
+            }
+
+            float g = gravity / distance;
+            if(distance > outerDistance)
+            {
+                g *= 1f - (distance - outerDistance) * outerFalloffFactor;
+            }
+
+            // 원본은 여기서 +g를 곱하는데, 방향 이슈가 생겨서 -g로 했습니다
+            return transform.TransformDirection(-g * vector);
+        }
+
         // 중심부 기준에서의 거리(크기)를 구함
         Vector3 distances;
         distances.x = boundaryDistance.x - Mathf.Abs(position.x);
@@ -130,6 +262,9 @@ public class GravityBox : GravitySource
 
         // 해당 메소드 초반에 InverseTransformDirection 했기 때문에 반대로 돌리는 것
         // return vector;
+
+        //Debug.Log("return inner gravity");
+
         return transform.TransformDirection(vector);
     }
 }
