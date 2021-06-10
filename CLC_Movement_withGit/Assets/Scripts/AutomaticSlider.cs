1fc0d54166a6ca966957f00e9aebed41cf1a05f7
HwangHyunJing <bbq2277@naver.com>
2021-06-10 15:39:10 +0900

    2.10 - 3.1, 3.2, 움직이는 플랫폼 활성화

---
+++ CLC_Movement_withGit/Assets/Scripts/AutomaticSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutomaticSlider : MonoBehaviour
+{
+    // 
+    [SerializeField, Min(0.01f)]
+    float duration = 1f;
+
+    // Unity는 제너릭 형을 직렬화하지 못하기 때문에, 별도의 클래스를 생성
+    [System.Serializable]
+    public class OnVAlueChangedEvent : UnityEvent<float> { }
+
+    [SerializeField]
+    OnVAlueChangedEvent onValueChanged = default;
+
+    float value;
+
+    private void FixedUpdate()
+    {
+        // 플랫폼의 위치에 대응되는 값을 변화
+        value += Time.deltaTime / duration;
+        if(value >= 1f)
+        {
+            value = 1f;
+            enabled = false;
+        }
+
+        // 변화된 값으로 Event에 넘김
+        onValueChanged.Invoke(value);
+    }
+}
---
+++ CLC_Movement_withGit/Assets/Scripts/PositionInterpolator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionInterpolator : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody body = default;
+
+    [SerializeField]
+    Vector3 from = default, to = default;
+
+    // from에서 to까지 t값에 따라서 보간해주는 메소드
+    public void Interpolate (float t)
+    {
+        body.MovePosition(Vector3.LerpUnclamped(from, to, t));
+    }
+}
